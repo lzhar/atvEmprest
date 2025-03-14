16f6088c77b6d3fad06cd0a93b6d1d161f5e21f3
LUIZ HENRIQUE ALVES RODRIGUES <luiz.hrodrigues@a.ucb.br>
2025-03-13 21:01:59 -0300

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var nomeDoCliente = document.getElementById("campoDeNome");
+function criarPessoa(event) {
+    event.preventDefault(); 
 
-var listaDeClientes = [];
+    nomeDoCliente = document.getElementById('nome').value;
+    cpfDoCliente = document.getElementById('cpf').value; 
+    valorDoEmprestimo = document.getElementById('valorDoEmprestimo').value;
+    taxaDeJurosAnual = document.getElementById('taxaDeJurosAnual').value;
+    qtdDeAnos = document.getElementById('qtdDeTempoEmMeses').value;
 
-function enviar(){
-    listaDeClientes.push(nomeDoCliente);
+    exbirValorTotalParaSerPago(qtdDeAnos, taxaDeJurosAnual, valorDoEmprestimo);
+    
 }
+
+function exbirValorTotalParaSerPago(qtdDeAnos, taxaDeJurosAnual, valorDoEmprestimo){
+    let valorTotal;
+
+    for(let i = 0; i <= qtdDeAnos; i++){
+        valorTotal = ((taxaDeJurosAnual/100) * valorDoEmprestimo) + valorDoEmprestimo;
+    }
+
+    return valorTotal;
+}
+
